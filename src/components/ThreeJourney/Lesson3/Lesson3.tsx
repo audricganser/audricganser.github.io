297ed0bc7bf8e866a4f60d1337572e27f48b4727
Audric Ganser <audricganser@gmail.com>
2026-03-25 00:39:27 -0500

    Lesson 3 mobile, extras

--- src/components/ThreeJourney/Lesson3/Lesson3.tsx
+++ src/components/ThreeJourney/Lesson3/Lesson3.tsx
@@ -19,11 +19,15 @@ const sceneContent = [
   { text: 'Renderer', url: 'https://threejs.org/docs/#Renderer' },
 ]
 
+const FIXED_HEIGHT = 600
+const PADDING = 40
+
 export const Lesson3 = () => {
   const [isWireFrame, setIsWireFrame] = useState(false)
 
   useEffect(() => {
     const canvas = document.querySelector('canvas.Lesson3WebGl')
+    const initialWidth = window.innerWidth - PADDING * 2
 
     const scene = new THREE.Scene()
     const geometry = new THREE.BoxGeometry(1, 1, 1)
@@ -34,12 +38,7 @@ export const Lesson3 = () => {
     const mesh = new THREE.Mesh(geometry, material)
     scene.add(mesh)
 
-    const sizes = {
-      width: 800,
-      height: 600,
-    }
-
-    const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+    const camera = new THREE.PerspectiveCamera(75, initialWidth / FIXED_HEIGHT)
     camera.position.z = 3
     scene.add(camera)
 
@@ -48,11 +47,31 @@ export const Lesson3 = () => {
     }
     const renderer = new THREE.WebGLRenderer({
       canvas: canvas,
+      alpha: true,
     })
 
-    renderer.setSize(sizes.width, sizes.height)
+    renderer.setSize(initialWidth, FIXED_HEIGHT)
+
+    const handleResize = () => {
+      const width = window.innerWidth - PADDING * 2
+      camera.aspect = width / FIXED_HEIGHT
+      camera.updateProjectionMatrix()
+      renderer.setSize(width, FIXED_HEIGHT)
+      renderer.render(scene, camera)
+    }
+
+    window.addEventListener('resize', handleResize)
 
     renderer.render(scene, camera)
+
+    console.log(renderer.info.programs?.length)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+      renderer.dispose()
+      geometry.dispose()
+      material.dispose()
+    }
   }, [isWireFrame])
 
   return (
@@ -66,7 +85,7 @@ export const Lesson3 = () => {
           using pure ThreeJS.
         </p>
         {sceneContent.map((t, idx) => (
-									<li key={t.text + idx}>
+          <li key={t.text + idx}>
             <a
               href={t.url}
               target="_blank"
@@ -75,6 +94,36 @@ export const Lesson3 = () => {
             </a>
           </li>
         ))}
+        <h3>Extras:</h3>
+        <p>
+          <strong>Wireframe Toggle:</strong> to show that this is a BoxGeometry
+          and not just a square
+        </p>
+        <p>
+          <strong>Resize Width:</strong> The lesson provided an object with set
+          height and width sizes. This didnt look good on mobile so I decided to
+          add an event listener to resize based off of{' '}
+          <code>window.innerWidth</code> and padding. These changes resulted in
+          some camera updates as well{' '}
+          <code>camera.updateProjectionMatrix()</code>. Calling the function
+          allows the camera to re-calculate its field of view.
+        </p>
+        <p>
+          <strong>Resource Management:</strong> Since WebGL doesn't
+          automatically garbage-collect GPU assets, I added the{' '}
+          <code>dispose()</code> method in the <code>useEffect</code> cleanup.
+          This manually frees up VRAM and prevents memory leaks when the
+          component unmounts or updates.
+          <br />
+          <a href="https://discourse.threejs.org/t/is-dispose-even-needed/3482">
+            "But you don't have to take my word for it" - LeVar Burton
+          </a>
+        </p>
+        <p>
+          <strong>Renderer Alpha:</strong> Set the renderer Alpha prop to true
+          so that my canvas scss color would come through. Setting it to true
+          allows the GPU to not set its default color on the canvas.
+        </p>
       </div>
       <div className={styles.canvas}>
         <Button onClick={() => setIsWireFrame((x) => !x)}>
--- src/routes/threeJourney.tsx
+++ src/routes/threeJourney.tsx
@@ -10,12 +10,13 @@ function RouteComponent() {
   return (
     <div className={styles.root}>
       <div className={styles.info}>
-        <h2>Hello ThreeJS!</h2>
+        <h1>Hello ThreeJS!</h1>
         <p>
-          First, a huge thank you to Bruno Simon and his course on ThreeJS{' '}
-          <a href="https://threejs-journey.com">(ThreeJS Journey)</a>. This page
-          is dedicated to showcasing the progress I'm making in the course, and
-          any comments I have had along the way.
+          First, a huge thank you to{' '}
+          <a href="https://bruno-simon.com/">Bruno Simon</a> and his course on
+          ThreeJS <a href="https://threejs-journey.com">(ThreeJS Journey)</a>.
+          This page is dedicated to showcasing the progress I'm making in the
+          course, and any comments I have had along the way.
         </p>
       </div>
       <Lesson3 />
